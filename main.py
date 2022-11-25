12adb02ddfbacc6ef62ea9b4dd46e421729ceff0
Gregory1108 <grisha.yurov@list.ru>
2022-11-25 23:41:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import sys
+
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("Ходилка-Бродилка")
+    bg_color = (0,150,50)
+
+run()
